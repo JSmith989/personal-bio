a78241eacc4ba554e3a9eb2b5258c579b821d879
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-11-29 16:07:27 -0600

    added some text under name

--- src/javascripts/components/intro.js
+++ src/javascripts/components/intro.js
@@ -1,17 +1,18 @@
 const myIntro = () => {
   const domString = `
   <div class="my-name"> Jordan Smith </div>
+  <div class="my-title"> Full Stack Developer </div>
   <div class="my-tech">
-  <i class="fab fa-html5 fa-2x fa-color p-3"></i>
-  <i class="fab fa-js-square fa-2x fa-color p-3"></i>
-  <i class="fab fa-bootstrap fa-2x fa-color p-3"></i>
-  <i class="fab fa-sass fa-2x fa-color p-3"></i>
-  <i class="fab fa-github fa-2x fa-color p-3"></i>
-  <i class="fab fa-css3-alt fa-2x fa-color p-3"></i>
-  <i class="fab fa-node-js fa-2x fa-color p-3"></i>
-  <i class="fab fa-git fa-2x fa-color p-3"></i>
-  <i class="fab fa-npm fa-2x fa-color p-3"></i>
-  <i class="fab fa-react fa-2x fa-color p-3"></i>
+  <i class="fab fa-html5 fa-2x fa-color-2 p-3"></i>
+  <i class="fab fa-js-square fa-2x fa-color-2 p-3"></i>
+  <i class="fab fa-bootstrap fa-2x fa-color-2 p-3"></i>
+  <i class="fab fa-sass fa-2x fa-color-2 p-3"></i>
+  <i class="fab fa-github fa-2x fa-color-2 p-3"></i>
+  <i class="fab fa-css3-alt fa-2x fa-color-2 p-3"></i>
+  <i class="fab fa-node-js fa-2x fa-color-2 p-3"></i>
+  <i class="fab fa-git fa-2x fa-color-2 p-3"></i>
+  <i class="fab fa-npm fa-2x fa-color-2 p-3"></i>
+  <i class="fab fa-react fa-2x fa-color-2 p-3"></i>
 
 
   </div>
